42c9c7cf3c67179d52db2a8f781a5784d2fbfc3d
Your Name <you@example.com>
2019-05-05 20:04:07 +0200

    DEBUT

--- src/main/java/com/example/demo/entity/git5.java
+++ src/main/java/com/example/demo/entity/git5.java
@@ -2,19 +2,6 @@ package com.example.demo.entity;
 
 public class git5 {
 public static void mymet(){
-	System.out.println("is 1");
-	System.out.println("is 2");
-	System.out.println("is 3");
-	System.out.println("is 4");
-	System.out.println("is 5");
-	System.out.println("is 6");
-	System.out.println("is 7");
-	System.out.println("is 8");
-	System.out.println("is 9");
-	System.out.println("is 10");
-	
-	System.out.println("i am yelhan");
-	System.out.println("i am ib 16");
-
+	System.out.println("debut");
 }
 }
